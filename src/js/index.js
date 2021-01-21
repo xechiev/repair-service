c3572845afedbe85c644c2c9c4963db9ebd667ea
Mingiian Khechiev <khechievm08@yandex.ru>
2021-01-21 23:27:25 +0300

    подключилыцшзук

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,39 @@
 import '../scss/style.scss';
 
-console.log('Works!');
+'use strict';
+const slider = document.querySelector('.swiper-container');
+let mySwiper;
+
+const btn = document.querySelector('.brand-button');
+
+function mobileSlider() {
+    if(window.innerWidth < 768 && slider.dataset.mobile === 'false') {
+        mySwiper = new Swiper(slider, {
+            slidesPerView: 'auto',
+            centeredSlides: true,
+            width: 240,
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+            },
+            keyboard: {
+                enabled: true,
+                onlyInViewport: true,
+            }
+        });
+        slider.dataset.mobile = 'true';
+    }
+    if(window.innerWidth >= 768 && window.innerWidth <= 1119) {
+        slider.dataset.mobile = 'false';
+
+        if(slider.classList.contains('swiper-container-initialized')) {
+            mySwiper.destroy();
+        }
+        
+    }
+}
+mobileSlider();
+
+window.addEventListener('resize', function() {
+    mobileSlider();
+});
