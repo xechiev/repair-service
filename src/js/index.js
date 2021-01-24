8dbfea015499c7582000ccdfe858a7616fb1cdb8
Mingiian Khechiev <khechievm08@yandex.ru>
2021-01-24 15:16:58 +0300

    Сделал price

--- src/js/index.js
+++ src/js/index.js
@@ -8,14 +8,14 @@ Swiper.use([Navigation, Pagination]);
 const slider = document.querySelector('.swiper-container');
 let mySwiper;
 
-const btn = document.querySelector('.brand-button');
+// const btn = document.querySelector('.brand-button');
 
 function mobileSlider() {
     if(window.innerWidth < 768 && slider.dataset.mobile === 'false') {
         mySwiper = new Swiper(slider, {
-            slidesPerView: 'auto',
+            slidesPerView: '1',
             centeredSlides: true,
-            width: 240,
+            // width: 240,
             pagination: {
                 el: '.swiper-pagination',
                 clickable: true,
@@ -40,3 +40,38 @@ mobileSlider();
 window.addEventListener('resize', function() {
     mobileSlider();
 });
+
+openMenu('.header__button-icon--burger', '.container__sidebar');
+openMenu('.footer__button-icon--call', '.container__modal-call');
+openMenu('.footer__button-icon--chat', '.container__modal-feedback');
+openMenu('.header__button-icon--chat', '.container__modal-feedback');
+openMenu('.header__button-icon--call', '.container__modal-call');
+
+closeMenu('.header__button-icon-burger', '.container__sidebar');
+closeMenu('.sidebar-container__fog', '.container__sidebar');
+
+closeMenu('.modal-feedback__button', '.container__modal-feedback');
+closeMenu('.modal-feedback__fog', '.container__modal-feedback');
+
+closeMenu('.modal-call__button', '.container__modal-call');
+closeMenu('.modal-call__fog', '.container__modal-call');
+
+function openMenu(selectorButton, selectorMenu) {
+  let selectorMenuActive = selectorMenu.trim() + '-active';
+  selectorMenuActive = selectorMenuActive.slice(1);
+  document.querySelector(selectorButton).addEventListener('click', function (evnt) {
+    evnt.preventDefault();
+    document.querySelector(selectorMenu).classList.add(selectorMenuActive);
+    document.querySelector('body').classList.add('body-active');
+  })
+}
+
+function closeMenu(selectorButton, selectorMenu) {
+  let selectorMenuActive = selectorMenu.trim() + '-active';
+  selectorMenuActive = selectorMenuActive.slice(1);
+  document.querySelector(selectorButton).addEventListener('click', function (evnt) {
+    evnt.preventDefault();
+    document.querySelector(selectorMenu).classList.remove(selectorMenuActive);
+    document.querySelector('body').classList.remove('body-active');
+  })
+}
